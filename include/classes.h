aa63926367baabff5b8dd7d0737145f82395ed78
matyko <lorinczi.matyas@student.ms.sapientia.ro>
2022-11-02 17:29:53 +0200

    class.h valtoztatas

--- include/classes.h
+++ include/classes.h
@@ -2,41 +2,37 @@
 #include "spells.h"
 #include "items.h"
 #include "effects.h"
-#include <string>
-#include <vector>
-
-
 
 class Item{
-	std::string name;
+    string name;
 };
 
 class Spell{
-	int Cast(Creature *caster, Creature* enemy) = 0;
+    int Cast(Creature *caster, Creature* enemy) = 0;
 };
 
 class Creature{
-	int hp,maxHp;
-	int strength; // [0, 10]
-	Team *team;
-	vector <Effect>;
+    int hp,maxHp;
+    int strength;
+    Team *team;
+    vector <Effect>;
 public:
-	int attack(Creature *enemy);
+    int attack(Creature *enemy);
 };
 
 class Humanoid: public Creature{
-	Armor *armor;
-	Weapon *weapon;
-	int castSpell(Spell*,Enemy*);
-	int useItem();
+    Armor *armor;
+    Tool *weapon;
+    int castSpell(Spell*,Enemy*);
+    int useItem();
 };
 
 class Player: public Humanoid{
-	
+
 };
 
 class Team{
-	std::string name;
-	std::vector<Team> allies;
-	std::vector<Team> enemies;
+    string name;
+    vector <Team> allies;
+    vector <Team> enemies;
 }
