953282a128b56c9eb0bc04edf1f86a9f36a1108c
Edebala <ede.bala4@protonmail.com>
2022-12-07 17:58:40 +0200

    Added Character declaration

--- include/Character.h
+++ include/Character.h
@@ -1,3 +1,15 @@
 #pragma once
 #include "Map.h"
 #include "creatures.h"
+
+enum DIRECTION(DIR_EAST,DIR_WEST,DIR_SOUTH,DIR_NORTH);
+
+class Character{
+	int posX,posY;
+	Creature* creature;
+public:
+	int getPosX();
+	int getPosY();
+	int move(int x,int y);
+	int walk(DIRECTION dir);
+};
