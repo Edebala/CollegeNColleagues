7bd925012d23d8b06cf74f87faf77522757b73d5
LorincziMatyas12 <matyas.lorinczi@gmail.com>
2022-12-14 09:47:25 +0200

    Formated the code for easier understanding in Business.cpp, the new functions were declared in Business.h

--- include/Business.h
+++ include/Business.h
@@ -1,8 +1,13 @@
 #include <string>
+#include "creature.h"
 
 using namespace std;
 
 int fight(Creature *player, Creature *enemy);
+
+bool useSpell(Creature *player, Creature *enemy);
+
+bool useItem(Creature *player, Creature *enemy);
 
 int getPlayersMove();
 
--- src/Business.cpp
+++ src/Business.cpp
@@ -1,94 +1,29 @@
 #include "../include/Business.h"
 
 int fight(Creature *player, Creature *enemy) {
-    bool roundNumber = 0;
     bool dead = false;
 
     while (true) {
-        ++roundNumber;
-
         // humaniod's turn
         if (!dead) {
             switch (getPlayersMove()) {
-                case 1: {
-                    if (player->attack(enemy)) { dead = true; }
+                case 1:
+                    dead = player->attack(enemy)}
                     break;
-                }
-                case 2: {
-                    switch (getSpellToCast()) {
-                        case 1: {
-                            Fireball *fire = new Fireball(1);
-                            if (fire->cast(human, creature)) { dead = true; }
-                            break;
-                        }
-                        case 2: {
-                            PoisonGas *poison = new PoisonGas(3);
-                            if (poison->cast(creature, human)) { dead = true; }
-                            break;
-                        }
-                        case 3: {
-                            Debuff *debuff = new Debuff(1, 20, 300);
-                            debuff->cast(human, creature);
-                            break;
-                        }
-                        case 4: {
-                            // buff == erase
-                            break;
-                        }
-                        default:
-                        {
-                            break;
-                        }
-                    }
+                case 2:
+                    dead = useSpell(player, enemy);
                     break;
-                }
-                case 3: {
-                    switch (getItemToUse()) {
-                        case 1: {
-                            Throwable * throwable = new Throwable("Shuriken", 4);
-                            throwable->use(creature);
-                            break;
-                        }
-                        case 2: {
-                            HealingPotion * healingPotion = new HealingPotion("Maiden tears", 1, 50);
-                            healingPotion->use(human);
-                            break;
-                        }
-                        case 3: {
-                            WeaponBuffPotion * equipmentBuffPotion = new WeaponBuffPotion("Rampage", 1, 60);
-                            equipmentBuffPotion->use(human->getWeapon());
-                            break;
-                        }
-                        case 4: {
-                            ArmorBuffPotion * armorBuffPotion = new ArmorBuffPotion("The best defense", 1, 50);
-                            armorBuffPotion->use(human->getArmor());
-                            break;
-                        }
-                        case 5: {
-                            StrengthenPotion * strengthenPotion = new StrengthenPotion("Lion heart", 1, 70);
-                            strengthenPotion->use(creature);
-                            break;
-                        }
-                        default:
-                        {
-                            break;
-                        }
-                    }
+                case 3:
+                    dead = useItem();
                     break;
-                }
                 default:
-                {
                     break;
-                }
             }
         }
 
         // creature/enemy's turn
-        if (!dead) {
-            if (enemy->attack(player)) {
-                dead = true;
-            }
-        }
+        if (!dead)
+            dead = enemy->attack(player);
     }
 }
 
@@ -126,4 +61,67 @@ int getItemToUse() {
     int itemNr = 1;
     // TODO: bind a key to the players choice
     return itemNr;
+}
+
+bool useSpell(Creature *player, Creature *enemy) {
+    bool dead = false;
+    switch (getSpellToCast()) {
+        case 1: {
+            Fireball *fire = new Fireball(1);
+            if (fire->cast(player, enemy)) { dead = true; }
+            break;
+        }
+        case 2: {
+            PoisonGas *poison = new PoisonGas(3);
+            if (poison->cast(player, enemy)) { dead = true; }
+            break;
+        }
+        case 3: {
+            Debuff *debuff = new Debuff(1, 20, 300);
+            debuff->cast(player, enemy);
+            break;
+        }
+        case 4: {
+            // buff == erase
+            break;
+        }
+        default: {
+            break;
+        }
+    }
+    return dead;
+}
+
+bool useItem(Creature *player, Creature *enemy) {
+    bool dead = false;
+    switch (getItemToUse()) {
+        case 1: {
+            Throwable *throwable = new Throwable("Shuriken", 4);
+            throwable->use(enemy);
+            break;
+        }
+        case 2: {
+            HealingPotion *healingPotion = new HealingPotion("Maiden tears", 1, 50);
+            healingPotion->use(player);
+            break;
+        }
+        case 3: {
+            WeaponBuffPotion *equipmentBuffPotion = new WeaponBuffPotion("Rampage", 1, 60);
+            equipmentBuffPotion->use(player->getWeapon());
+            break;
+        }
+        case 4: {
+            ArmorBuffPotion *armorBuffPotion = new ArmorBuffPotion("The best defense", 1, 50);
+            armorBuffPotion->use(player->getArmor());
+            break;
+        }
+        case 5: {
+            StrengthenPotion *strengthenPotion = new StrengthenPotion("Lion heart", 1, 70);
+            strengthenPotion->use(player);
+            break;
+        }
+        default: {
+            break;
+        }
+    }
 }
